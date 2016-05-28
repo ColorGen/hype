4ee44420895dbe41050755a9af40390bafad5947
Julien Cornut <julien.cornut@hesge.ch>
2016-05-28 16:01:18 +0200

    change port

--- server.js
+++ server.js
@@ -153,5 +153,5 @@ var server_port = process.env.OPENSHIFT_NODEJS_PORT || 80
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
 app.listen(server_port, server_ip_address, function () {
-console.log( "Listening on " + server_ip_address + ", server_port " + server_port )
+console.log( "Listening on " + server_ip_address + ", server_port " + port )
 });
